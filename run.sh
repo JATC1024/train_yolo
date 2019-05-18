d503fdaa05b657c4a865beb87ccd997faaff9142
JATC1024 <JATC1024@gmail.com>
2019-05-18 23:04:49 +0700

    add codes

--- run.sh
+++ run.sh
@@ -3,12 +3,16 @@ echo "Cloning the yolo repository"
 git clone https://github.com/AlexeyAB/darknet.git
 cd darknet
 
+echo "Copying all the prepared files"
+cp ../prepared_files/* .
 
-echo "Editing the Makefile"
-cp ../prepared_files/Makefile Makefile
+echo "Make file"
 make
 
 
-echo "Getting pretrained weights"
-wget https://pjreddie.com/media/files/yolo.weights
-./darknet detect cfg/yolo.cfg yolo.weights data/dog.jpg
+#echo "Getting pretrained weights"
+#wget https://pjreddie.com/media/files/yolo.weights
+#./darknet detect cfg/yolo.cfg yolo.weights data/dog.jpg
+
+echo "Creating train.txt and test.txt"
+python prepare_train_test_file.py
